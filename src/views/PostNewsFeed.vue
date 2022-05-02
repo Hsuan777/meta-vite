3ceaadcc0651e9de960bdd0471f68caf745bf2ba
Vic <syuan0212@gmail.com>
2022-05-02 19:42:26 +0800

    add image upload to imgul

--- src/views/PostNewsFeed.vue
+++ src/views/PostNewsFeed.vue
@@ -1,23 +1,69 @@
 <template>
   <div>
     <h2 class="text-center border border-dark border-2 py-4 bg-white mb-4
-          position-relative border-overlap border-overlap-start fw-bold">張貼動態</h2>
+      position-relative border-overlap border-overlap-start fw-bold">張貼動態</h2>
     <div class="p-8 shadow-lg border border-dark border-2 rounded bg-white">
       <!-- eslint-disable-next-line -->
       <label for="newsFeedTextarea" class="mb-1 d-block">貼文內容</label>
-      <textarea name="" id="newsFeedTextarea"
+      <textarea v-model="inputContent" name="" id="newsFeedTextarea"
         cols="30" rows="10" class="w-100 mb-4 border border-dark border-2"></textarea>
-      <input type="button" value="上傳圖片" class="btn btn-dark px-8 py-1 mb-4">
-      <img src="" alt="" class="d-none mb-8">
+      <input ref="imageFile" type="file" class="d-none btn btn-dark px-8 py-1 mb-4" @change="uploadImageToImgul($event)">
+      <input type="button" value="上傳圖片" class="btn btn-dark px-8 py-1 mb-4" @click="imageFile.click()">
+      <div v-if="imageInfo.link" class="mb-8">
+        <img :src="imageInfo.link" :alt="imageInfo.name" class="image-fluid">
+      </div>
       <div class="d-flex justify-content-center">
         <input type="button" class="w-50 mx-auto py-4 btn btn-secondary bg-black-50"
-          value="送出貼文">
+          value="送出貼文" @click="postData" :disabled="!inputContent">
       </div>
     </div>
   </div>
 </template>
 <script>
-
+import {ref, reactive} from 'vue';
+import axios from 'axios';
 export default {
+  setup() {
+    const apiurl = "https://metawall.herokuapp.com/posts";
+    const inputContent = ref("");
+    const imageFile = ref(null);
+    const imageInfo = reactive({});
+    const imgul = {
+      id: "e7bdb6b252f9fe5",
+    } 
+    const uploadImageToImgul = (e) => {
+      const file = e.target.files[0];
+      let form = new FormData();
+      let settings = {
+        method: "POST",
+        url: "https://api.imgur.com/3/image",
+        headers: {
+          Authorization: `Client-ID ${imgul.id}`
+        },
+        mimeType: "multipart/form-data"
+      };
+      form.append("image", file);
+      form.append("name", file.name);
+      settings.data = form;
+      axios(settings).then((res) => {
+        imageInfo.link = res.data.data.link;
+        imageInfo.name = res.data.data.name;
+        e.target.value = "";
+      })
+    }
+    const postData = () => {
+      const newPost = {
+        user: "626def88402e0ab428973045",
+        image: imageInfo.link,
+        content: inputContent.value
+      }
+      axios.post(apiurl, newPost).then((res) => {
+        if (res.data.status ===  200) {
+          inputContent.value = "";
+        }
+      })
+    }
+    return {inputContent, imageInfo, imageFile, uploadImageToImgul, postData}
+  }
 };
 </script>
