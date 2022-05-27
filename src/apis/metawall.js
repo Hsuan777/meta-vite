e292df4ab133ed61ff46b4d3f7b99d0d206b36d8
Vic <syuan0212@gmail.com>
2022-05-27 11:05:21 +0800

    refactor: 分類 API

--- src/apis/metawall.js
+++ src/apis/metawall.js
@@ -1,13 +1,22 @@
 import req from './https';
 
+// posts
 export const apiGetPosts = (query) => req('get', `posts?${query}`);
 export const apiGetPost = (id) => req('get', `posts/${id}`);
-export const apiGetUserProfile = () => req('get', 'user/profile');
 export const apiPostPosts = (params, settings) => req('post', 'posts', params, settings);
+
+// posts > likes
 export const apiPostLikes = (id) => req('post', `posts/${id}/likes`);
+
+// user
+export const apiGetUserProfile = () => req('get', 'user/profile');
 export const apiPostUserPassword = (params) => req('post', 'user/updatePassword', params);
 export const apiPatchUserProfile = (params, settings) => req('patch', 'user/profile', params, settings);
-export const apiTPSignup = (params, settings) => req('post', 'user/TPSignup', params, settings);
+
+// user sign_in
 export const apiSignin = (params) => req('post', 'user/signin', params);
-export const apiTPSignin = (params, settings) => req('post', 'user/TPSignin', params, settings);
 export const apiCheckSignin = (params) => req('get', 'user/check', params);
+
+// third party sign_in/sign_up
+export const apiTPSignup = (params, settings) => req('post', 'user/TPSignup', params, settings);
+export const apiTPSignin = (params, settings) => req('post', 'user/TPSignin', params, settings);
