70a0a20d20e5c702b28c5266aeacd5656f90fba1
Vic <syuan0212@gmail.com>
2022-06-02 17:50:52 +0800

    feat: 張貼動態圖片檢查

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -4,9 +4,12 @@
 
   const inputContent = ref("");
   const imageFile = ref(null);
-  const imageInfo = reactive({});
   const submitMessage = ref("");
-  
+  const imagePreviewInfo = reactive({
+    imagesBase64: [],
+    errorMessage: [],
+  })
+
   const postData = async () => {
     const userId = await apiGetUserProfile().then((res) => res.data.data._id);
     const photos = Array.from(imageFile.value.files);
@@ -16,7 +19,6 @@
     })
     form.append("user", userId);
     form.append("content", inputContent.value);
-    imageInfo.link = ""
     inputContent.value = "";
     apiPostPosts(form, {mimeType: "multipart/form-data"}).then((res) => {
       if (res.data.status ===  "success") {
@@ -24,6 +26,31 @@
       }
     })
   }
+  const toUploadImages = ($event) => {
+    imagePreviewInfo.imagesBase64.length = 0;
+    imagePreviewInfo.errorMessage.length = 0;
+    const currentImages = Array.from($event.target.files);
+    const checkImageSize = currentImages.some((item) => item.size >=  2*1024*1024);
+    if (currentImages.length > 10) {
+      imagePreviewInfo.errorMessage.push('檔案數量不能超過 10 張');
+      imagePreviewInfo.imagesBase64.length = 0;
+    }
+    if (checkImageSize) {
+      imagePreviewInfo.errorMessage.push('檔案大小需在 2 MB 以內');
+      imagePreviewInfo.imagesBase64.length = 0;
+    }
+    if (currentImages.length <= 10 && !checkImageSize) {
+      currentImages.forEach((item) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(item);
+        reader.onload = ($event) => {
+          imagePreviewInfo.imagesBase64.push($event.target.result);
+        };
+      })
+    }
+  };
+
+
   watch(inputContent, (newValsue) => {
     submitMessage.value = "";
   })
@@ -37,11 +64,14 @@
       <label for="newsFeedTextarea" class="mb-1 d-block">貼文內容</label>
       <textarea v-model="inputContent" name="" id="newsFeedTextarea"
         cols="30" rows="10" class="w-100 mb-4 border border-dark border-2"></textarea>
-      <input ref="imageFile" type="file" name="photos" class="d-none btn btn-dark px-8 py-1 mb-4" multiple="multiple">
+      <input @change="toUploadImages($event)" ref="imageFile" type="file" name="photos" class="d-none btn btn-dark px-8 py-1 mb-4" multiple="multiple">
       <input type="button" value="上傳圖片" class="btn btn-dark px-8 py-1 mb-4" @click="imageFile.click()">
-      <div v-if="imageInfo.link" class="mb-8">
-        <img :src="imageInfo.link" :alt="imageInfo.name" class="img-fluid">
+      <div v-if="imagePreviewInfo.imagesBase64.length > 0" class="d-flex justify-content-center flex-wrap align-items-center mb-8">
+        <img v-for="(item, index) in imagePreviewInfo.imagesBase64" :key="`image${index}`" :src="item" :alt="`image${index}`" class="img-fluid w-25 h-25 mb-2 mx-3">
       </div>
+      <p v-if="imagePreviewInfo.errorMessage.length > 0" class="d-flex flex-column align-items-center text-danger">
+        <span v-for="(msg, index) in imagePreviewInfo.errorMessage" :key="index">{{index+1 +'. ' + msg}}</span>
+      </p>
       <div class="d-flex justify-content-center">
         <input type="button" class="w-50 mx-auto py-4 btn btn-secondary bg-black-50"
           value="送出貼文" @click="postData" :disabled="!inputContent">
