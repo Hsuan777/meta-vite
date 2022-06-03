3228063a859ef61e6e005bbdd2062ae5fa2fec71
Vic <syuan0212@gmail.com>
2022-06-03 15:48:27 +0800

    deploy

---
+++ assets/auth.b5844a25.js
@@ -0,0 +1,1 @@
+import{g as t}from"./index.33f9b0d7.js";const o=t("Auth",{state:()=>({user:{}}),getters:{},actions:{}});export{o as a};
--- assets/auth.b636787e.js
+++
@@ -1,1 +0,0 @@
-import{g as t}from"./index.15b5e9bb.js";const o=t("Auth",{state:()=>({user:{}}),getters:{},actions:{}});export{o as a};
